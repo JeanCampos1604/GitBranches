0e4f7ecd69da700a8c2d503a660a85b8f883c8c3
JeanCampos <jeanflaviodecampos1604@gmail.com>
2020-10-22 16:53:43 -0300

    Commit SimpleCalculator.c

---
+++ SimpleCalculator.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    printf("Calculadora simples(+,-,/,*)\n");
+    float num1,num2,result;
+    char op;
+    printf("Digite a conta da seguinte forma: Número Operador Número\n");
+    printf("Valores: ");
+    scanf("%f %c %f", &num1,&op,&num2);
+
+    switch(op)
+    {
+    case '+':
+        result = num1 + num2;
+    break;
+
+    case '-':
+        result = num1 - num2;
+    break;
+
+    case '*':
+        result = num1 * num2;
+    break;
+
+    case '/':
+        if (num1==0 || num2==0) {
+        	printf("Não existe divisão por 0");
+        	return;
+		} else {
+			result = num1 / num2;
+		}
+    break;
+
+    }
+
+
+    printf("Resultado = %.2f", result);
+
+    return 0;
+
+    }
